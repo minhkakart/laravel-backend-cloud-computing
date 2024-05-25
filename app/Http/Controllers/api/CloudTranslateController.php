b88776fb1706a310ac2759d64f109a94f007dcd0
minhkakart <nguyenminh2732@gmail.com>
2024-05-25 21:56:22 +0700

    test add route

--- app/Http/Controllers/api/CloudTranslateController.php
+++ app/Http/Controllers/api/CloudTranslateController.php
@@ -30,6 +30,7 @@ class CloudTranslateController extends Controller
     public function create()
     {
         //
+        return 'create';
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\api\CloudTranslateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/translate', [CloudTranslateController::class, 'index']);
+Route::get('/create', [CloudTranslateController::class, 'create']);
 
 Route::get('/', function () {
     return view('welcome');
