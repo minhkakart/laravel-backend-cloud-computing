c15ee1cb2da43421de33b4507d0cee3cb3a75d09
minhkakart <nguyenminh2732@gmail.com>
2024-05-25 21:06:18 +0700

    test route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\api\CloudTranslateController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('translate', CloudTranslateController::class);
+Route::get('/translate', [CloudTranslateController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
