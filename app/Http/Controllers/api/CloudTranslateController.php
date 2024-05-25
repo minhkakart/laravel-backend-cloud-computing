9f9d2c32a2884764a7f2374d6557d5c4529b39c9
minhkakart <nguyenminh2732@gmail.com>
2024-05-25 20:22:10 +0700

    test translate api

---
+++ app/Http/Controllers/api/CloudTranslateController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Google\Cloud\Translate\V2\TranslateClient;
+
+class CloudTranslateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $translate = new TranslateClient([
+            'suppressKeyFileNotice' => true,
+            'key' => env('GOOGLE_CLOUD_TRANSLATE_API_KEY')
+        ]);
+        $languages = $translate->localizedLanguages([
+            'target' => 'vi'
+        ]);
+        return $languages;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\api\CloudTranslateController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('translate', CloudTranslateController::class);
 
 Route::get('/', function () {
     return view('welcome');
