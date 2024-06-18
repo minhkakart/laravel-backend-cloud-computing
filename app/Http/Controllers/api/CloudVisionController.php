a031a76d5cb7a50921861c912de56ce53ebcaace
Nhat37 <164352743+Nhat37@users.noreply.github.com>
2024-06-18 21:20:42 +0700

    Update CloudVisionController.php

--- app/Http/Controllers/api/CloudVisionController.php
+++ app/Http/Controllers/api/CloudVisionController.php
@@ -52,4 +52,127 @@ class CloudVisionController extends Controller
         $client->close();
         return response()->json(['textdetect' => $result]);
     }
+    ////////////
+    public function faceDetection(Request $request)
+    {
+        $image_url = $request->input('gcs_uri');
+
+        $client = new ImageAnnotatorClient();
+
+        $response = $client->faceDetection($image_url);
+        $faceAnnotations = $response->getFaceAnnotations();
+
+        $faces = [];
+        if ($faceAnnotations) {
+            foreach ($faceAnnotations as $faceAnnotation) {
+                $likelihoods = [
+                    'joyLikelihood' => Likelihood::name($faceAnnotation->getJoyLikelihood()),
+                    'sorrowLikelihood' => Likelihood::name($faceAnnotation->getSorrowLikelihood()),
+                    'angerLikelihood' => Likelihood::name($faceAnnotation->getAngerLikelihood()),
+                    'surpriseLikelihood' => Likelihood::name($faceAnnotation->getSurpriseLikelihood()),
+                    'underExposedLikelihood' => Likelihood::name($faceAnnotation->getUnderExposedLikelihood()),
+                    'blurredLikelihood' => Likelihood::name($faceAnnotation->getBlurredLikelihood()),
+                    'headwearLikelihood' => Likelihood::name($faceAnnotation->getHeadwearLikelihood()),
+                ];
+                $faces[] = $likelihoods;
+            }
+        }
+
+        $client->close();
+
+        return response()->json(['faces' => $faces]);
+    }
+
+    public function labelDetection(Request $request)
+    {
+        $image_url = $request->input('gcs_uri');
+
+        $client = new ImageAnnotatorClient();
+
+        $response = $client->labelDetection($image_url);
+        $labelAnnotations = $response->getLabelAnnotations();
+
+        $labels = [];
+        if ($labelAnnotations) {
+            foreach ($labelAnnotations as $labelAnnotation) {
+                $labels[] = $labelAnnotation->getDescription();
+            }
+        }
+
+        $client->close();
+
+        return response()->json(['labels' => $labels]);
+    }
+
+    public function safeSearchDetection(Request $request)
+    {
+        $image_url = $request->input('gcs_uri');
+
+        $client = new ImageAnnotatorClient();
+
+        $response = $client->safeSearchDetection($image_url);
+        $safeSearchAnnotation = $response->getSafeSearchAnnotation();
+
+        $safeSearch = [];
+        if ($safeSearchAnnotation) {
+            $safeSearch = [
+                'spoofLikelihood' => Likelihood::name($safeSearchAnnotation->getSpoof()),
+                'medicalLikelihood' => Likelihood::name($safeSearchAnnotation->getMedical()),
+                'violenceLikelihood' => Likelihood::name($safeSearchAnnotation->getViolence()),
+                'racyLikelihood' => Likelihood::name($safeSearchAnnotation->getRacy()),
+            ];
+        }
+
+        $client->close();
+
+        return response()->json(['safeSearch' => $safeSearch]);
+    }
+
+    public function logoDetection(Request $request)
+    {
+        $image_url = $request->input('gcs_uri');
+
+        $client = new ImageAnnotatorClient();
+
+        $response = $client->logoDetection($image_url);
+        $logoAnnotations = $response->getLogoAnnotations();
+
+        $logos = [];
+        if ($logoAnnotations) {
+            foreach ($logoAnnotations as $logoAnnotation) {
+                $logos[] = $logoAnnotation->getDescription();
+            }
+        }
+
+        $client->close();
+
+        return response()->json(['logos' => $logos]);
+    }
+
+    public function imagePropertyDetection(Request $request)
+    {
+        $image_url = $request->input('gcs_uri');
+
+        $client = new ImageAnnotatorClient();
+
+        $response = $client->imagePropertiesDetection($image_url);
+        $imagePropertiesAnnotation = $response->getImagePropertiesAnnotation();
+
+        $properties = [];
+        if ($imagePropertiesAnnotation) {
+            $dominantColors = $imagePropertiesAnnotation->getDominantColors()->getColors();
+            foreach ($dominantColors as $colorInfo) {
+                $color = $colorInfo->getColor();
+                $properties[] = [
+                    'red' => $color->getRed(),
+                    'green' => $color->getGreen(),
+                    'blue' => $color->getBlue(),
+                ];
+            }
+        }
+
+        $client->close();
+
+        return response()->json(['properties' => $properties]);
+    }
 }
